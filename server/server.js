6d356d21579e7b2868e9fe19a9c06e7cb5ae2c97
madhusudhanrao beesetty <madhusudhan@madhusudhan.hsd1.ca.comcast.net>
2020-08-10 00:08:59 -0700

    'intro to graphQL'

--- server/server.js
+++ server/server.js
@@ -1,7 +1,38 @@
 console.log('server is starting.....');
-
-var express = require('express');
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+} = require('graphql')
 
 var app = express();
 
-var sever = app.listen(3000);
+const quiz = [
+  { id: 1, name: 'J. K. Rowling' }
+]
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'Root Query',
+  fields: () => ({
+    quiz: {
+      description: 'topic for the quiz',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: () => quiz
+    },
+  })
+})
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}))
+app.listen(5000, () => console.log('Server Running'))
